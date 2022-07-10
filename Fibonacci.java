7e088fcf19b027c4d7da3b4ac95c0b2e3c783bc1
Sriram9011 <109014794+Sriram9011@users.noreply.github.com>
2022-07-10 12:15:19 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 11 , firstTerm =0, secondTerm = 1;
+		 System.out.println("Fibonacci series till "+ n+"terms");
+		 
+		for (int i =1; i <=n;++i) {
+			System.out.println(firstTerm);
+			
+			int nextTerm = firstTerm + secondTerm;
+		      firstTerm = secondTerm;
+		      secondTerm = nextTerm;
+			
+		}
+		 
+		 
+
+	}
+
+}
